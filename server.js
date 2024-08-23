75697a71c584131bca4fbf8ac6cc061ee81d8737
Sahul <sahulb2016@gmail.com>
2024-08-23 00:06:04 -0400

    comment for adding your own api key server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/chat', async (req, res) => {
             messages: [{ role: "user", content: userMessage }]
         }, {
             headers: {
-                'Authorization': `Bearer sk-3rmLMsRK-E2W5g_DQpJ9ccuVJdeHsAy97lIRzjGwKST3BlbkFJSALZ1dOU6DyWAPS2c7oJnsc-XL6TLK3smTvS7MuDwA`, 
+                'Authorization': `Bearer //add your own API key here`, 
             }
         });
 
